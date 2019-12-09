f9c0b1aa7fc3259844bc8e5eae9306b08df44a74
Brittany Houtz <brittany.houtz@gmail.com>
2019-12-08 17:55:20 -0800

    added request handler stuff

--- server.js
+++ server.js
@@ -6,8 +6,8 @@ function start(route) {
         let pathname = url.parse(request.url).pathname;
         console.log('Request for " + pathname + " received.');
 
-        route(pathname);
-        
+        route(handle, pathname);
+
         response.writeHead(200, { 'Content-Type': 'text/plain' });
         response.write('Hello World');
         response.end();
@@ -16,5 +16,5 @@ function start(route) {
     http.createServer(onRequest).listen(8888);
     console.log('Server has started.');
 }
-exports.start = start;
-    
+
+exports.start = start;    
